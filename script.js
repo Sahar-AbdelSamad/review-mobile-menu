be08bd62d339bd632e7f402dd8047846c868f13d
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-05-19 13:13:30 +0300

    Create mobule menu

---
+++ script.js
@@ -0,0 +1,15 @@
+const openMenu = document.querySelector('.menu-bars');
+const mobileMenu = document.querySelector('#mobile-nav');
+const closeMenu = document.querySelector('#mobile-nav');
+
+function show() {
+  mobileMenu.style.display = 'flex';
+  mobileMenu.style.top = '0';
+}
+
+function close() {
+  mobileMenu.style.top = '-100%';
+}
+
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close); 
